0a41ac60ce0c57f04090d449b5d0f9d87bc2f4d9
Evgeny <Zheka-grid@yandex.ru>
2022-05-26 09:53:28 +0300

    Add page uris

---
+++ src/main/java/com/hrydziushka/finalproject/controller/PageUri.java
@@ -0,0 +1,8 @@
+package com.hrydziushka.finalproject.controller;
+
+public final class PageUri {
+
+    public static final String HOME_PAGE_URI ="/controller?command=go_to_home_page";
+    public static final String SIGN_UP_PAGE_URI ="/controller?command=go_to_sign_up_page";
+    private PageUri(){}
+}
